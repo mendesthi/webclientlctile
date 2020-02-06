dc5bf7960b2885c8e339e9a62d633187ef256928
yatseali <yatseali@users.noreply.github.com>
2020-02-07 00:07:24 +1100

    initial commit

---
+++ package/FioriContainerApp/controller/Container.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	'jquery.sap.global',
+	"sap/ui/core/mvc/Controller"
+], function (jQuery,Controller) {
+	"use strict";
+    
+	return Controller.extend("xxx.FioriContainerApp.controller.Container", {
+		onInit: function () {
+		}
+	});
+});
