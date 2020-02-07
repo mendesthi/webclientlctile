f9f41b656d2bbaf0d2d793f748f738f5088e9cbb
yatseali <yatseali@users.noreply.github.com>
2020-02-07 16:59:18 +1100

    update version

--- package.sh
+++ package.sh
@@ -1,3 +1,5 @@
 #package the extension and copy to package folder for deployment
-zip -r package/package.zip src/ -x "*__MACOSX" -x "*.DS_Store" -x "package.zip"
-cp src/package.json package/
+cd src
+rm -f ../package/*
+zip -r ../package/package.zip ./ -x "*__MACOSX" -x "*.DS_Store" -x "package.zip"
+cp package.json ../package/
